f032131bce510f82cbcab465a3edbdef650c1223
Belkacem Alidra <belkacem.alidra@gmail.com>
2015-06-24 17:47:42 +0200

    Clean + Typo

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,13 +1,8 @@
 Alloy.Globals.config = require('config/default').config;
-//Alloy.Globals.theme	 = require('themes/flat_blue').theme;
 Alloy.Globals.theme	 = require('themes/clear').theme;
 
 var menu = Alloy.createWidget('ds.slideMenu');	
 $.container.add(menu.getView());
 menu.init();
-		
-function doClick(e) {
-    alert($.label.text);
-}
 
 $.index.open();
